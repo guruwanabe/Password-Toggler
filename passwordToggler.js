5760042adbf6b07c218d3a716e28248467f85363
guruwanabe <bp.stanciu@gmail.com>
2017-01-30 00:09:08 +0200

    Update passwordToggler.js
    codacy review 3

--- passwordToggler.js
+++ passwordToggler.js
@@ -22,7 +22,7 @@
           if(typeof $objSelector != "undefined" || $objSelector == null){
                 //Bail early if selector not of type password
                 if($objSelector.type !== "password"){
-                  console.log('Error: selector is not of type password');
+                  //console.log("Error: selector is not of type password");
                   return false;
                 }else{
                   if (this.blnCreated === true) {
